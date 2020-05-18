d9ad21b5a6523dbcfecb92202833eddc458f1d7e
sasami55 <sasami55@users.noreply.github.com>
2020-05-18 16:50:02 +0900

    create new

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+// game start
+
+document.querySelector('.game-start').addEventListener('click' , function() {
+  document.querySelector('#wrapper-start').style.display = 'none';
+  document.querySelector('#wrapper-game').style.display = 'flex';
+});
